576e371f9d0e3ab0a98437003e69cfed5f1436ce
h0bbs99 <jdoakes13@gmail.com>
2024-05-29 20:51:04 -0500

    Changed main to include function...commented out count letters

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
+    #Counts the number of words
+def count_words(text):
+        word = text.split()
+        print(len(word))
+        
+        return (len(word))
+    
+
 def main():
+    
     
     letters = {}
     words = 0
-    
+    file_contents =[]
+
     # Counts the total number of words in a text
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        words = file_contents.split()
     
-    print(len(words))
-
+    words = count_words(file_contents)
+    
+    
     # Counts the total number of each word and adds it to a dicctionary
 
-    for word in words:
-        text = word.lower()
-        text = text.split("")
+    #for word in words:
+     #   text = word.lower()
+      #  text = text.split()
         
-        print(text)
-
+       # print(f"Text: {text}")
         #for letter in text:
          #   if letter == letters{letter}:
 
