accd87a8038b3ac29c805596b2685fec213a2d82
h0bbs99 <jdoakes13@gmail.com>
2024-06-08 19:54:24 -0500

    Clean Print function added

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
     #Counts the number of words
 def count_words(text):
-        word = text.split()
-        print(len(word))
-        
-        return (len(word))
+    word = text.split()
+              
+    return (len(word))
     
 
 # Counts to the letters of each script and returns to lower case
@@ -19,12 +18,24 @@ def count_characters(text):
               
     return character
 
+# Converts dictionary to dictionary of dictionaies for character counts.
 def convert_to_list(char_dict):
     char_list =  []
     for letter, count in char_dict.items():
         char_list.append({"character": letter, "num": count})
     
     return char_list
+
+# Allows for sorting characters by number
+def sort_on(dict):
+    return dict["num"]
+
+#Cleanly prints the results of the book
+def clean_print(words, char):
+    print(f"{words} words are found in the document\n\n")
+
+    for i in char:
+        print(f"The '{i["character"]}' was found {i["num"]} times")
 
 
                
@@ -52,9 +63,15 @@ def main():
         words = count_words(file_contents)
 
         letters = count_characters(file_contents)
-        
 
-        print(letters)
+        letters = convert_to_list(letters)
+        
+        letters.sort(reverse=True, key=sort_on)
+
+        clean_print(words, letters)
+
+
+        
 
    
     
